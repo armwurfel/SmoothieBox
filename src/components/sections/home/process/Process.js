d59a4bedc6d184698151b0deaaf0ca7d976b2faa
armwurfel <arm.wurfel@gmail.com>
2019-11-23 07:55:35 +0500

    Home Process Section Updated

--- src/components/sections/home/process/Process.js
+++ src/components/sections/home/process/Process.js
@@ -10,9 +10,20 @@ class Process extends Component {
                 <Container>
                     <Row>
                         <Col md={6}>
-                            <h2>Hinn fullkomni smoothie á 2 mínútum</h2>
+                            <h2 className="heading mb-5 pb-4">Hinn fullkomni smoothie á 2 mínútum</h2>
+                            <Row className="m-0" >
+                                <Col md={9}>
+                                    <ol className="process-type">
+                                        <li className="mb-5">Tear open your Crop’s Smoothie pack and tip the fruit into the blender.</li>
+                                        <li className="mb-5">Splash in 250 ml of unsweetened juice (cloudy apple works best) or cow, soy, almond milk or water.</li>
+                                        <li className="mb-5">Blend for 60 seconds.</li>
+                                    </ol>
+                                </Col>
+                            </Row>
                         </Col>
-                        <Col md={6}></Col>
+                        <Col md={6}>
+                            <img className="process-img" src="./assets/images/smoothie-fruit.jpg" alt=""/>
+                        </Col>
                     </Row>
                 </Container>
             </section>
